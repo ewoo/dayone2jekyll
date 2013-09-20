5fa289367545115ecc411c01056ec19fc362a82e
Wooyong Ee <wooyong@gmail.com>
2013-09-20 18:06:14 -0400

    Implement Entry class

--- dayone-to-jekyll.py
+++ dayone-to-jekyll.py
@@ -4,6 +4,7 @@
 import os
 import re
 from os.path import expanduser
+from dateutil import parser
 
 def main():
   home = expanduser("~")
@@ -20,22 +21,59 @@ def main():
     if match:
       count = count + 1
       if count > 1:
-          # dump entry
-          print count
-          process_entry(entry)
-          # new entry init.
-          entry = []
+        #process_entry(entry)
+        e = Entry(entry)
+        #e.print_headers()
 
-    entry.append(line)
-  
+        # Init entry
+        entry = []
+      entry.append(line)
   fo.close()
 
 
 #  print lastline
+class Entry:
+  def __init__(self, raw_content):
+    self.raw_content = raw_content
+    self.headers = []
+    self.create_date = False
+    self.title = ""
+    self.short_title = ""
+    self.file_name = ""
+    self.content = ""
+    self.image = ""
+    self.__parse_content()
+
+  def __parse_content(self):
+    firstline = self.raw_content[0]
+    self.create_date = parser.parse(firstline.replace("Date:", ""))
+
+    header_format = re.compile("^\t\w+:\t")
+    for line in self.raw_content:
+      print line
+      match = header_format.search(line)
+      if match:
+        self.headers.append(line)
+      else:
+        self.content.append(line)
+
+    # Massage headers. Remove beginning tabs and make lower case.
+    #    for line in self.headers:
+
+  def print_headers(self):
+    for line in self.headers:
+      print line
+
+
 
 def process_entry(entry):
+  firstline = entry[0].replace("Date:", "")
+  print firstline
+  ds = parser.parse(firstline)
+
   print entry[0]
-  savefile(entry)
+  print ds.strftime("%Y-%m-%d") # %H:%M:%S
+  savefile("", "content")
 
 def build_filename_from_entry(entry):
   # Format: 2013-09-13-first-sentence-or-heading.md
